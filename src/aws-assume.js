91d6e0d20fac6d7f7bcd7dfffe536858cc7dccdc
cayasso <cayasso@gmail.com>
2016-11-30 10:43:20 -0600

    remove makefile and LICENSE use npm for build

--- src/aws-assume.js
+++ src/aws-assume.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 'use strict'
-
+import 'babel-polyfill'
 import { SharedIniFileCredentials, STS } from 'aws-sdk'
 import { join, dirname } from 'path'
 import { readFileSync } from 'fs'
